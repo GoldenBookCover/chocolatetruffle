bf66794c557a7f238ebb8217a51cf1bc4df2da52
Monstre Charmant <ballonrage@gmail.com>
2020-07-14 00:04:18 +0200

    feat: add setup.py
    - restructure files
    - can install via setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chocolatetruffle", # Replace with your own username
+    version="1.0.0",
+    author="Monstre Charmant",
+    author_email="ballonrage@gmail.com",
+    description="Useful tool-kit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MonstreCharmant/chocolatetruffle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
